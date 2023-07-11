b22a148868d75066b7687672b07ae960fbac6b9a
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-07-11 17:34:27 +0900

    add:Updateの追加

--- cruds/todo.go
+++ cruds/todo.go
@@ -17,6 +17,15 @@ func GetAllTodos() (res_todos []db.Todo, err error) {
 	return
 }
 
+// Todoを更新する関数
+func UpdateTodo(id uint, content string) (err error) {
+	if err = db.Sspl.Where("id = ?", id).Error; err != nil {
+		return err
+	}
+	db.Sspl.Model(&Todo).Where("id = ?", id).Update(content, "Content")
+	return
+}
+
 // 引数のidのTodoを削除する関数
 func DeleteTodo(id uint) (err error) {
 	err = db.Sspl.Where("id = ?", id).Delete(&db.Todo{}).Error
