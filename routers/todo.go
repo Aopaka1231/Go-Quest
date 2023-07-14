3ae454818e728e5e1785178be6f91184e714a9d3
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-07-14 15:50:44 +0900

    add:編集機能の追加

--- routers/todo.go
+++ routers/todo.go
@@ -10,27 +10,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//TodoRouterを初期化する関数
-func InitTodoRouters(tr *gin.RouterGroup){
+// TodoRouterを初期化する関数
+func InitTodoRouters(tr *gin.RouterGroup) {
 	//""というパスに対してGETメソッドで、getTodos関数を設定する
 	tr.GET("", getTodos)
 	//これも同様
 	tr.POST("", postTodo)
 	//"/:id"とすることで任意の文字列をidが拾ってこれるようになる
 	tr.DELETE("/:id", deleteTodo)
+	tr.PATCH("/:id", UpdateTodo)
 }
 
-//データベースからTodoの配列を受け取りそれをフロントに返す関数
-func getTodos(c *gin.Context){
+// データベースからTodoの配列を受け取りそれをフロントに返す関数
+func getTodos(c *gin.Context) {
 	var (
 		todos []db.Todo
-		err error
+		err   error
 	)
 
 	//crudsで定義したGetAllTodosを使用
 	if todos, err = cruds.GetAllTodos(); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"message":err.Error(),
+			"message": err.Error(),
 		})
 		return
 	}
@@ -39,12 +40,12 @@ func getTodos(c *gin.Context){
 	c.JSON(http.StatusOK, &todos)
 }
 
-//フロントから送られてきた情報をデータベースに保存する関数
-func postTodo(c *gin.Context){
-	var(
-		err error
+// フロントから送られてきた情報をデータベースに保存する関数
+func postTodo(c *gin.Context) {
+	var (
+		err     error
 		payload schema.CreateTodo
-		todo db.Todo
+		todo    db.Todo
 	)
 
 	//BindJSONは構造体とjsonを結び付けてくれる
@@ -69,7 +70,7 @@ func postTodo(c *gin.Context){
 	c.JSON(http.StatusOK, &todo)
 }
 
-func deleteTodo(c *gin.Context){
+func deleteTodo(c *gin.Context) {
 	//c.Param("id")として、パスパラメータを取得できる
 	//でもこれは文字列なのでuint型に変換したい
 	//strconvパッケージを使用して10進数で、64bitのuintに変換
@@ -94,4 +95,38 @@ func deleteTodo(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"message": "OK!!",
 	})
+}
+
+func UpdateTodo(c *gin.Context) {
+	var (
+		err     error
+		payload schema.CreateTodo
+		todo    db.Todo
+	)
+
+	if err = c.BindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	id := c.Param("id")
+	parseId, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	if todo, err = cruds.UpdateTodo(uint(parseId), payload.Content); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	//errorがなければ登録したtodoを返す
+	c.JSON(http.StatusOK, &todo)
 }
